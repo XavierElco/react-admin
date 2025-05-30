c0ed899fd2b31fbf276027b742f2e776a489349d
XavierElco <ccaihong@icloud.com>
2025-05-30 18:48:49 +1000

    拉起项目

---
+++ router/index.tsx
@@ -0,0 +1,5 @@
+import { createBrowserRouter } from "react-router-dom";
+import Login from '@/views/Login';
+import Welcome from '@/views/Welcome';
+import Error404 from '@/views/404';
+import Error403 from '@/views/403';
